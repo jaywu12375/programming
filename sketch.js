8d7dcbd05d32fc7785e5525a6fca2c746af0bc06
jaywu12375 <37837961+jaywu12375@users.noreply.github.com>
2018-04-24 09:08:40 -0400

    first upload moon

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var t = 255; //variable for the text's transparency which will change based on the user's action
+var p = 0; //count of points
+var numOfpoints = 7 //how many points we want
+
+//The setup function only happens once
+function setup() {
+	createCanvas(500, 500); //create a 500px X 500px canvas
+}
+
+//The draw function happens over and over again
+function draw() {
+  background(50,50,50); //an RGB color for the canvas' background (dark blue)
+  //moon
+  fill(135,206,250,200); //white, 200 level of transparency (min = 0, max = 255)
+  noStroke();
+  ellipse(width/2, height/2,400,400); //center of the canvas, 400px dia
+  //text
+  textSize(20);
+  textAlign(RIGHT);
+  textFont("Palatino");
+  fill(255,255,51,t); // yellow, t refers to the transparency (set to fully opaque (255) at the beginning (see variable a the top of the sketch))
+  text("hello",width-20, height-20);
+  if(mouseIsPressed) //making the text disappear when the mouse is down
+  	t = t-3; // decrease the opacity of the text (see variable at the top of the sketch)
+  	if (t<=0){ //if the text is completely transparent, reset it
+  		t=255;
+  	}
+  //firefly
+  fill(255,200); //white, semi-transparent
+  ellipse(mouseX,mouseY,25,25); // follows the mouse, 10px dia
+  //firefly2
+  fill(255,200); //white, semi-transparent
+  ellipse(mouseX,mouseY,50,50); // follows the mouse, 10px dia
+  //sparkly sky (points are redrawn randomly on the canvas everytime we go through the draw function
+  stroke(255);
+  //loop for 7 points 
+  for (p = 0; p < numOfpoints; p ++){
+  	point(random(0,width),random(0,height));
+  }
+
+
+}
